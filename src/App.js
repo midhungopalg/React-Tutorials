2e330f089292461ebe4886d22d8f582585a86e6f
Midhun G <84282922+midhungopalg@users.noreply.github.com>
2022-12-18 13:01:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import UseEffectComponent from "./useEffect/UseEffectComponent";
 import UseReducerComponent from "./useReducer/UseReducerComponent";
 import UseRefComponent from "./useRef/UseRefComponent";
 import UseStateComponent from "./useState/UseStateComponent";
-//https://github.com/machadop1407/react-hooks-course/tree/master/src
 
 function App() {
   return (
